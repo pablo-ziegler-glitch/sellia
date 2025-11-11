52ad47ac75921952af8a7f79112c343fdc3ec3c1
pablo-ziegler-glitch <pabloz18ezeiza@gmail.com>
2025-11-10 22:25:13 -0300

    Unify Kotlin version to 2.1.10 and fix plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,12 @@
-buildscript {
-    dependencies {
-        classpath(libs.hilt.android.gradle.plugin)
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Updated Kotlin and plugin versions
 plugins {
-    kotlin("jvm") version "2.1.10" apply false
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
-    id("org.jetbrains.kotlin.kapt") version "2.1.0"
-    id("com.google.gms.google-services") version "4.4.3" apply false
+    kotlin("jvm") version "2.1.10"
+    id("some.plugin.id") version "some.plugin.version"
+}
 
-    // ⬇️ Hilt plugin disponible para los módulos (no se aplica aquí)
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+// other configurations
+kotlin {
+    jvmToolchain { 
+        languageVersion.set("2.1.10")
+    }
 }
